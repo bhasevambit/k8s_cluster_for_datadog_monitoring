ebb6cd03438af628fb2443210ea1d9c34def313e
bhasevambit <jammadoibit6123@gmail.com>
2023-04-15 16:26:20 +0900

    Retry before datadog connection

---
+++ api_image_build.sh
@@ -0,0 +1,8 @@
+#/usr/bin/bash
+
+cd ./api
+docker build . -t sample_api_for_k8s_datadog_monitoring
+docker tag sample_api_for_k8s_datadog_monitoring bhasevambit/sample_api_for_k8s_datadog_monitoring:latest
+docker push bhasevambit/sample_api_for_k8s_datadog_monitoring:latest
+
+cd ..
