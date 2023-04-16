87defe4426d527c691cff5b235b697057cf644b8
bhasevambit <jammadoibit6123@gmail.com>
2023-04-16 15:50:50 +0900

    Update Datadog agent shell scripts

--- Datadog_Agent_upgrade.sh
+++ Datadog_Agent_upgrade.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
+# --- minikube向けの「Unable to detect the kubelet URL automatically(キューブレット URL を自動的に検出できません)」エラー対策 ---
+DD_KUBELET_TLS_VERIFY=false
+
+# --- helm用いたDatadog Agent更新 ---
 helm upgrade datadog -f datadog-values.yaml --set datadog.site='ap1.datadoghq.com' --set datadog.apiKey='${DD_API_KEY}' --set datadog.appKey='${DD_APP_KEY}' datadog/datadog 
